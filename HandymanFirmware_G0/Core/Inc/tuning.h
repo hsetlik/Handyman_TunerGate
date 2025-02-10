e344cf67e9234fac3344cf13786155a715058f2f
Hayden Setlik <haydensetlik@gmail.com>
2025-02-09 23:13:09 -0700

    tuning error logic

--- HandymanFirmware_G0/Core/Inc/tuning.h
+++ HandymanFirmware_G0/Core/Inc/tuning.h
@@ -18,20 +18,25 @@ extern "C" {
 #define SEMITONE_RATIO 1.05946309436f
 #define IN_TUNE_THRESHOLD 4
 #define TICK_RATE 4000
+#define NUM_MIDI_NOTES 127
 
 #define TICK_OVERFLOW_SAFE
-
-// call this on the rising edge interrupt for the TUNE_IN pin
-void tuning_rising_edge();
-// get the current pitch in hz
-float tuning_get_current_hz();
-
 
 // store the nearest note and error in cents
 typedef struct {
 	uint8_t note;
-	int8_t error;
-};
+	int16_t cents;
+} tuning_error_t;
+
+
+
+// call this on the rising edge interrupt for the TUNE_IN pin
+void tuning_rising_edge();
+
+void tuning_update_error(tuning_error_t* err);
+
+
+
 
 
 
--- HandymanFirmware_G0/Core/Src/tuning.c
+++ HandymanFirmware_G0/Core/Src/tuning.c
@@ -9,6 +9,8 @@
 // shared vars
 static uint32_t tuningBuf[TUNING_BUF_SIZE];
 static uint16_t head = 0;
+static uint8_t pitchesInitialized = 0;
+static float midiNotePitches[NUM_MIDI_NOTES];
 
 void tuning_rising_edge(){
 	tuningBuf[head] = *TICK_COUNT;
@@ -39,4 +41,65 @@ float tuning_get_current_hz(){
 	return (totalMs / (float)(TUNING_BUF_SIZE - 1)) / 1000.0f;
 }
 
+// note/error stuff-------------------
+void initMidiPitches(){
+	for(uint8_t note = 0; note < NUM_MIDI_NOTES; ++note){
+		midiNotePitches[note] = 440.0f * powf(SEMITONE_RATIO, (float)note - 69.0f);
+	}
+}
 
+// get the note nearest to a given pitch in hz
+uint8_t closestNote(float hz){
+	if(hz <= midiNotePitches[0])
+		return 0;
+	if(hz >= midiNotePitches[NUM_MIDI_NOTES - 1])
+		return midiNotePitches[NUM_MIDI_NOTES - 1];
+	// binary search
+	uint8_t i = 0;
+	uint8_t j = NUM_MIDI_NOTES;
+	uint8_t mid = 0;
+	while (i < j) {
+		mid = (i + j) / 2;
+		// check left half
+		if(hz < midiNotePitches[mid]) {
+			if(mid > 0 && hz >= midiNotePitches[mid - 1]) {
+				if(hz - midiNotePitches[mid - 1] >= midiNotePitches[mid] - hz)
+					return mid - 1;
+				return mid;
+			}
+			j = mid;
+		} else { // check right half
+			if(mid < (NUM_MIDI_NOTES - 1) && hz < midiNotePitches[mid + 1]) {
+				if(hz - midiNotePitches[mid] >= midiNotePitches[mid + 1] - hz)
+					return mid;
+				return mid + 1;
+			}
+			i = mid + 1;
+		}
+	}
+	return mid;
+}
+
+// get the error in cents between the note's pitch and the given frequency
+int16_t getErrorHz(uint8_t midiTarget, float hz){
+	if(hz <= midiNotePitches[midiTarget]){ // we're flat
+		float diff = midiNotePitches[midiTarget] - hz;
+		float halfstep = midiNotePitches[midiTarget] - midiNotePitches[midiTarget - 1];
+		return (int16_t)((diff / halfstep) * -100.0f);
+	} else {
+		float diff = hz - midiNotePitches[midiTarget];
+		float halfstep = midiNotePitches[midiTarget + 1] - midiNotePitches[midiTarget];
+		return (int16_t)((diff / halfstep) * 100.0f);
+	}
+}
+
+void tuning_update_error(tuning_error_t* err){
+	if(!pitchesInitialized){
+		initMidiPitches();
+		pitchesInitialized = 0xFF;
+	}
+	float pitch = tuning_get_current_hz();
+	err->note = closestNote(pitch);
+	err->cents = getErrorHz(err->note, pitch);
+
+}
