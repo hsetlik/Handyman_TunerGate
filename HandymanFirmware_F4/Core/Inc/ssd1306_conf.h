91c41ab90ae10688115531313ce7b99c627b7fcb
Hayden Setlik <haydensetlik@gmail.com>
2026-03-25 13:24:36 -0600

    firmware flashing on rev2 board wooo

--- HandymanFirmware_F4/Core/Inc/ssd1306_conf.h
+++ HandymanFirmware_F4/Core/Inc/ssd1306_conf.h
@@ -38,7 +38,7 @@
 //#define SSD1306_Reset_Pin       OLED_Res_Pin
 
 // Mirror the screen if needed
-// #define SSD1306_MIRROR_VERT
+#define SSD1306_MIRROR_VERT
 // #define SSD1306_MIRROR_HORIZ
 
 // Set inverse color if needed
